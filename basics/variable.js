19e0f5fcd4ffd87edcc7b31c9d77249025eff747
Shitalbhai0412 <shitalbhairam299@gmail.com>
2025-02-17 11:23:43 +0000

    variables

---
+++ basics/variable.js
@@ -0,0 +1,17 @@
+let Name="shital"
+const idnod=1234
+let age=22
+let accountNo=2334556
+let phoneNo=276549876
+let accountEmail="shitalbhairam227@gmail.com"
+let accountCity="Nagpur"
+let accountState="Maharastra"
+let accountno;
+let state=""
+
+
+
+console.log(accountEmail)
+
+console.log(Name)
+console.table([Name,age,accountEmail,accountCity, accountState,accountno,state])
