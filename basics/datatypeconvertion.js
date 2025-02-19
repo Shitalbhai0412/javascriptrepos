b98d18d8e8b04dce753ec8e2ea5ed02585f811ba
Shitalbhai0412 <shitalbhairam299@gmail.com>
2025-02-19 08:43:48 +0000

    datatypeconvertion1

--- basics/datatypeconvertion.js
+++ basics/datatypeconvertion.js
@@ -5,6 +5,13 @@ let valueInNumber=Number (score)
 console.log(valueInNumber)
 console.log(typeof valueInNumber)
 
+let marks="19eae"
+console.log(marks)
+console.log(typeof marks)
+let value=Number(marks)
+console.log( typeof value)
+console.log(value)
+
 
 
 
