462a3b4f5b0646df9fa6e5ce8890b417dd7e9cc6
Shitalbhai0412 <shitalbhairam299@gmail.com>
2025-02-19 09:42:18 +0000

    Opration

---
+++ basics/opration.js
@@ -0,0 +1,31 @@
+let value=4
+let NegValue= -value
+console.log (NegValue)
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2%2);
+
+let str1="Sita"
+let str2="Ram"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1" + 2);
+
+console.log(1 + "2");
+
+console.log(1 + 2+"2");
+
+console.log("1"+2+2);
+
+// increment opration
+// prefix=>value increment before incrementing
+// postfix=>value increment after  incrementing
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter)
+
+
