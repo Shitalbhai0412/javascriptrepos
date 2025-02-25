999025f7c98b7d26df292627b2912576ee90af21
Shitalbhai0412 <shitalbhairam299@gmail.com>
2025-02-25 11:04:20 +0000

    String

---
+++ basics/string.js
@@ -0,0 +1,7 @@
+// string is an object it cantain the key value pair value
+const name="shital"
+const value=50
+console.log(`hello my name is${name} and the value is ${value}`);
+ 
+// 2 nd type of declaration of string in javascript
+const gameName= new String("Shitalbhai")
